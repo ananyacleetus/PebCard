b8d7e1f2001cef74d4c1430002bf90377cdaa188
Ananya W Cleetus <ananyacleetus@gmail.com>
2014-03-02 02:49:40 -0500

    Update resources.c
    SDK uniformity?

--- resources.c
+++ resources.c
@@ -1,27 +1,6 @@
-/*
-%%%%%%%%%%%%%%%%%%%%%
-Pebble Photo Watch Face
-Copyright (C) 2013 Corey Klass
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/layericenses/>.
-%%%%%%%%%%%%%%%%%%%%%
-
-- Look for the tag #CK for where to modify the watchface for your own custom images
-
-*/
-
-#include "pebble_os.h"
+#include "pebble_os.h" //sdk version?
 #include "pebble_app.h"
 #include "pebble_fonts.h" 
 #include "resources.c"
